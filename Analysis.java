bb11b71d6c4e8310af7c00097f12213cc090beb4
Mitchell Luhm <luhmx009@umn.edu>
2017-10-21 13:49:36 -0500

    Adding fake interface

---
+++ Analysis.java
@@ -0,0 +1,13 @@
+
+class Analysis {
+
+	/* findSchoolScore()
+	 * findGPA()
+	 * findNumberWorkExperiences()
+	 * findYearsOfExperience()
+	 * findSkillSimilarity()
+	 */
+
+	public static void main(String[] args) { }
+
+}
